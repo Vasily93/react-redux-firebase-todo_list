578213e92f9656f88ba83d17b209fa6ad6b4b149
Vasily93 <markovavasily@gmail.com>
2019-12-20 19:00:52 -0800

    deleed unnecesery files. updated serviceWorker file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  redner() {
+
+    return (
+      <div>
+      <h1>Hello World</h1>
     </div>
-  );
+    );
+  }
 }
 
 export default App;

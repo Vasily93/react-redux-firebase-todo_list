966d066a2f3e5bb5c09f04cfbac37baa28aae20f
Vasily93 <markovavasily@gmail.com>
2019-12-20 19:39:07 -0800

    creted store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>,
+     document.getElementById('root')    
+  </Provider>
+);
 
 registerServiceWorker();
